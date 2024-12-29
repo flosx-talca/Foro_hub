9c13241316e398a14b836a90123d84bf7e2de16c
Orlando Rozas <orlandorozasi@gmail.com>
2024-12-28 23:15:25 -0300

    Proyecto listo para empezar

---
+++ forohub/src/main/java/com/aluracursos/forohub/ForohubApplication.java
@@ -0,0 +1,22 @@
+package com.aluracursos.forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(ForohubApplication.class, args);
+
+		System.out.println("hola mundosdryhtrytr");
+		System.out.println("hola");
+
+	}
+
+
+
+
+}
