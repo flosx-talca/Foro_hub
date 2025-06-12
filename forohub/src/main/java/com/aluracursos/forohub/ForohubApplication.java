2de458ae6179738358ce8e7592383143fa5c7d2a
flosx-talca <orlandorozasi@gmail.com>
2025-06-11 23:26:12 -0400

    Update ForohubApplication.java

--- forohub/src/main/java/com/aluracursos/forohub/ForohubApplication.java
+++ forohub/src/main/java/com/aluracursos/forohub/ForohubApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ForohubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ForohubApplication.class, args);
+		SpringApplication.run(ForohubApplication.class, args );
 
 	}
 }
